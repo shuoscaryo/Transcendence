0b93d9cef31bf6dbd00d7d77c09161fbb781df76
shuoscaryo <shuoscaryo@gmail.com>
2024-10-27 00:50:39 +0200

    [main]<2024-10-27-00:50> if django project doesn't exist, create it inside the docker

--- docker/django/init.sh
+++ docker/django/init.sh
@@ -7,6 +7,12 @@ if [ -z "$POSTGRES_DB" ] || [ -z "$POSTGRES_USER" ] || [ -z "$POSTGRES_PASSWORD"
 	exit 1
 fi
 
+# Check if Django project is already created
+if [ ! -f "/app/manage.py" ]; then
+    echo "Django project not found. Creating a new Django project..."
+    django-admin startproject pong_project /app
+fi
+
 # Wait for database
 echo "Connecting to database..."
 while ! pg_isready -h postgresql -p 5432 -U $POSTGRES_USER -d $POSTGRES_DB; do
