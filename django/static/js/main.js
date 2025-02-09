b4daaf5b6318ba9e246438ef8f0976eccf60bff5
shuoscaryo <shuoscaryo@hgmail.com>
2025-02-09 23:15:14 +0100

    [main]<2025-02-09-23:15> feat: URL more or less working

--- django/static/js/main.js
+++ django/static/js/main.js
@@ -4,15 +4,17 @@ function router() {
     const currentURL = new URL(window.location.href);
     const parts = currentURL.pathname.split('/').filter(part => part.length > 0);
 
-    if (currentURL.pathname == '/')
+    if (currentURL.pathname == '/'
+        || currentURL.pathname == '/pages/'
+    )
         loadPage('main', 'home');
-    else if (parts[1] == 'pages') {
+    else if (parts[0] == 'pages') {
         const page = parts[1];
         const view = parts[2]; // TODO check if exists and choose default option
         loadPage(page, view);
     }
-    else
-        loadPage('error', '404'); // TODO: 404 page
+    //else
+        //loadPage('error', '404'); // TODO: 404 page
 }
 
 window.addEventListener('popstate', router);
