c74a67d4389324f58809dcca305a109405e4c5d1
shuoscaryo <shuoscaryo@hgmail.com>
2025-03-30 19:20:59 +0200

    refactor: separate values sent to other users from values sent to self. (username or email is not received by unlogged/not_me users)

--- django/pong_project/views/profile.py
+++ django/pong_project/views/profile.py
@@ -3,19 +3,27 @@ from django.contrib.auth import get_user_model
 
 CustomUser = get_user_model()
 
-def get_profile(display_name, authenticated=False):
+def get_profile(display_name, own_profile=False):
     try:
         user = CustomUser.objects.get(display_name=display_name)
-        profile = {
+        # Start with general data for everyone
+        output_data = {
             'display_name': user.display_name,
-            'email': user.email if authenticated else None,
             'wins': user.wins,
             'losses': user.losses,
             'profile_photo': user.profile_photo.url if user.profile_photo else '',
-            'friends': list(user.friends.values_list('display_name', flat=True)),
             'date_joined': user.date_joined,
         }
-        return JsonResponse(profile)
+        # Specific data for own profile
+        if own_profile:
+            own_profile_data = {
+                "username": user.username,
+                "email": user.email,
+                "friends": [friend.display_name for friend in user.friends.all()],
+            }
+            output_data.update(own_profile_data)
+
+        return JsonResponse(output_data)
     except CustomUser.DoesNotExist:
         return JsonResponse({'error': 'User not found'}, status=404)
     except Exception as e:
@@ -24,7 +32,7 @@ def get_profile(display_name, authenticated=False):
 def profile(request):
     if not request.user.is_authenticated:
         return JsonResponse({'error': 'Not authenticated'}, status=401)
-    return get_profile(request.user.display_name, authenticated = True)
+    return get_profile(request.user.display_name, own_profile = True)
 
 def profile_by_display_name(request, display_name):
-    return get_profile(display_name, authenticated = False)
+    return get_profile(display_name, own_profile = False)
